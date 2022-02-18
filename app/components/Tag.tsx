3964beda6f0845f2af53331ffe006dcc06a2b41f
0xNSKY <namhj315@gmail.com>
2022-02-18 22:55:55 +0900

    fix Tag component

--- app/components/Tag.tsx
+++ app/components/Tag.tsx
@@ -1,7 +1,7 @@
 export default function Tag({ text }: { text: string }) {
   return (
     <>
-      <div className="flex justify-center items-center bg-orange-400 rounded-xl w-12 h-6 text-white text-sm font-base mr-1">
+      <div className="flex justify-center items-center bg-orange-400 rounded-xl w-11 h-5 text-white text-xs font-base mr-1">
         {text}
       </div>
     </>
