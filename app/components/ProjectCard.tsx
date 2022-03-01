3fc9ac7b49b816150d5d1f3bac475dcb7c2aa783
0xNSKY <namhj315@gmail.com>
2022-03-01 21:15:29 +0900

    fix: writer name repair

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -4,9 +4,14 @@ import Tag from "./Tag";
 export default function ProjectCard({ project }: any) {
   return (
     <>
-      <li className="flex flex-col w-64 h-80 shadow-xl rounded-2xl bg-white overflow-hidden mb-12">
+      <li className="transition ease-in-out flex flex-col w-64 h-80 shadow-xl rounded-2xl bg-white overflow-hidden mb-12 hover:scale-110 duration-300">
         <Link to={project.id}>
-          <div className="bg-green-500 h-48"></div>
+          <div className="relative h-48 flex bg-gray-50 justify-center items-center flex-col">
+            <img src="./icon_photo.png" className="opacity-10 w-auto h-auto" />
+            <span className="bg-black text-white pl-2 pr-2 absolute left-4 bottom-4 font-semibold text-sm">
+              {project.name}
+            </span>
+          </div>
         </Link>
         <section className="flex flex-row p-4 justify-between items-center">
           <section className="flex flex-row flex items-center">
@@ -27,12 +32,18 @@ export default function ProjectCard({ project }: any) {
         <section className="relative flex flex-row items-center pl-4 pr-4">
           <img src="./icon_profile.png" className="w-9 h-9 mr-2" />
           <section className="flex flex-col ">
-            <p className="font-semibold text-sm h-5">{project.writer}</p>
+            <p className="font-semibold text-sm h-5">
+              {project.writer.username}
+            </p>
             <p className="text-xs opacity-50">{project.introduce}</p>
           </section>
+          <a
+            href={project.link}
+            className="absolute right-0 mr-6 w-7 h-7 z-10"
+          ></a>
           <img
             src="./icon_link.png"
-            className="absolute right-0 mr-6 cursor-pointer w-7 h-7 opacity-50"
+            className="absolute right-0 mr-6 w-7 h-7 opacity-50"
           />
         </section>
       </li>
